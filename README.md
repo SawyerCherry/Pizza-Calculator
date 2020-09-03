c5ca9f25de883821601f27324dccf2f404a16c78
Sawyer Cherry <sawyeracherry@gmail.com>
2020-09-03 12:23:26 -0500

    I commented the code and updated the readme.md file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Pizza-Calculator
-the 
+
+Name: Sawyer Alec Cherry
+
+Project Name: Pizza Calculator
+
+Programming Language Used: Python 3.7.9
+
+Date: 3-9-2020
+
+The pizza calculator takes in the amount of People and the amount of Pizza you and returns the amount of Pizza slices that each person will get. I created this because I have seen people actually fight over slices, if everyone ate their fair share of pizza, then no one would fight about it.
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-
+#get name
 name = input("What is your name?")
+
+# just some basic encouragement 
 
 print("That is a cool name, " + name)
 
-
+# user input for how many pizzas we have
 
 pizzas = int(input("How many pizzas do you have?"))
 
-
+# user input for how many people sharing pizza
 
 people = int(input("How many people are there?"))
 
+# get's the amount of pizza slices, since your basic pizza has 8 pieces.
 
 slices = pizzas * 8
 
+# this performs our basic operation to get our amount of slices per person
 
 slices_per_person = slices / people
 
-print("The results are in... :)")
+print("The results are in. :)")
+
+# for this I used an f-string this displays how many slices of pizza each person gets
 
 print(f"There are {slices_per_person} piece/pieces of pizza for each of your {people} people.")
 
